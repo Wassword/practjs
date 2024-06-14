8c040411b7b1f8505c61aef90587176e29d4b4d4
Wassword <ahmedsaba271@gmail.com>
2024-06-14 11:26:34 -0400

    added changes

---
+++ pract.js
@@ -0,0 +1,11 @@
+//Create a private variable named tasks to store task descriptions
+
+
+//global scope
+let globalVar= "I am global";
+
+//call the public method
+
+countReset.increment();//count 1
+console.log(counter.increment); //undefined
+
